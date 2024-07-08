84e3b6c041e5026f6c06a3cc042a5089db7271fc
Lucas Rosa <lucasmetron@gmail.com>
2024-07-08 17:34:13 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const foo = 'teste'
   return (
     <main className={styles.main}>
       <div className={styles.description}>
