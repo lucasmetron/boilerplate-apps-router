a7ccf57074267d06dda0dcb683093d68db004ff6
Lucas Rosa <lucasmetron@gmail.com>
2024-07-11 16:38:39 -0300

    teste

--- src/components/Main/Main.spec.tsx
+++ src/components/Main/Main.spec.tsx
@@ -6,10 +6,8 @@ describe('Main', () => {
   it('should render the heading', () => {
     render(<Main />)
 
-    // expect(
-    //   screen.getByRole('heading', { name: /react avançado/i })
-    // ).toBeInTheDocument(),
-
-    expect(screen.getByText('React avançado'))
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
   })
 })
