e8afc57105469d49c8c9a2064f55bbc546705f38
Lucas Rosa <lucasmetron@gmail.com>
2024-07-11 15:53:34 -0300

    teste

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'eslint --fix',
-    'prettier --write',
-    'jest --bail --passWithNoTests --detectOpenHandles'
-  ]
-}
