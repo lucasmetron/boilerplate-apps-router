3a725fbefa798639ba1e91d5dd53d13784514254
Lucas Rosa <lucasmetron@gmail.com>
2024-07-11 16:35:00 -0300

    teste

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'jest --bail --detectOpenHandles --passWithNoTests',
-    'eslint --fix',
-    'prettier --write'
+  // Lint & Prettify TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write "${filenames.join('" "')}"`,
+    `npm run lint --fix . "${filenames.join('" "--file "')}"`,
+    `npm test -- --findRelatedTests "${filenames.join('" "')}"`
   ]
 }
